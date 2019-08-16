9609482d72324ee5c049d7a8c733414dd2b2f3cd
Andrei Medvedev <super.user.maa@gmail.com>
2019-08-16 21:06:54 +0300

    Implemented TextFormattingRunProperties Gadget by Oleksandr Mirosh and Alvaro Munoz and added few patterns for binary formatter

--- DeReviewer.KnowledgeBase/Cases/BinaryFormatterPatterns.cs
+++ DeReviewer.KnowledgeBase/Cases/BinaryFormatterPatterns.cs
@@ -1,55 +1,101 @@
-using System.Runtime.Serialization.Formatters.Binary;
 using DeReviewer.KnowledgeBase.Formatters;
 using DeReviewer.KnowledgeBase.Gadgets;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DeReviewer.KnowledgeBase.Cases
 {
-    public class BinaryFormatterPatterns : Case
+  public class BinaryFormatterPatterns : Case
+  {
+    public void DeserializeTypeConfuseDelegate()
     {
-        public void Deserialize()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.Deserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>()));
-        }
-        
-        public void DeserializeHeaderHandler()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.Deserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null));
-        }
-        
-        public void DeserializeMethodResponse()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.DeserializeMethodResponse(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null,
-                    null));
-        }
-        
-        public void UnsafeDeserialize()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.UnsafeDeserialize(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null));
-        }
-
-        public void UnsafeDeserializeMethodResponse()
-        {
-            var serializer = new BinaryFormatter();
-            Pattern.CreateBySignature(it =>
-                serializer.UnsafeDeserializeMethodResponse(
-                    it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
-                    null,
-                    null));
-        }
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.Deserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>()));
     }
+
+    public void DeserializeTextFormattingRunProperties()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.Deserialize(
+          it.IsPayloadOf<TextFormattingRunProperties>().Format<Binary>()));
+    }
+
+    public void DeserializeHeaderHandlerTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.Deserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null));
+    }
+
+    public void DeserializeHeaderHandlerTextFormattingRunProperties()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.Deserialize(
+          it.IsPayloadOf<TextFormattingRunProperties>().Format<Binary>(),
+          null));
+    }
+
+    public void DeserializeMethodResponseTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.DeserializeMethodResponse(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null,
+              null));
+    }
+
+    public void DeserializeMethodResponseTextFormattingRunProperties()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.DeserializeMethodResponse(
+          it.IsPayloadOf<TextFormattingRunProperties>().Format<Binary>(),
+          null,
+          null));
+    }
+
+    public void UnsafeDeserializeTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.UnsafeDeserialize(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null));
+    }
+
+    public void UnsafeDeserializeTextFormattingRunProperties()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.UnsafeDeserialize(
+          it.IsPayloadOf<TextFormattingRunProperties>().Format<Binary>(),
+          null));
+    }
+
+    public void UnsafeDeserializeMethodResponseTypeConfuseDelegate()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+          serializer.UnsafeDeserializeMethodResponse(
+              it.IsPayloadOf<TypeConfuseDelegate>().Format<Binary>(),
+              null,
+              null));
+    }
+
+    public void UnsafeDeserializeMethodResponseTextFormattingRunProperties()
+    {
+      var serializer = new BinaryFormatter();
+      Pattern.CreateBySignature(it =>
+        serializer.UnsafeDeserializeMethodResponse(
+          it.IsPayloadOf<TextFormattingRunProperties>().Format<Binary>(),
+          null,
+          null));
+    }
+  }
 }
